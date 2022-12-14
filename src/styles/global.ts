916365dbdb353224070f5e51963fb3fe0822f3ed
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-14 15:11:03 -0300

    feat: global style

---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  body, html {
+    background-color: #100E1D;
+    font-family: 'Raleway', sans-serif;
+    color: #E7E7EB;
+    font-size: 16px;
+    font-weight: 500;
+  }
+`;
