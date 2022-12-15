e1d7a66dd489200d92e278f9d7206497fa6fdd54
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-15 16:23:27 -0300

    feat: add isMainContent prop

--- src/components/WeatherInformation/index.tsx
+++ src/components/WeatherInformation/index.tsx
@@ -3,13 +3,14 @@ import * as S from './styles';
 interface WIProps {
   title: string;
   subtitle: string;
+  isMainContent?: boolean;
 }
 
-const WeatherInformation = ({title, subtitle}: WIProps) => {
+const WeatherInformation = ({title, subtitle, isMainContent = false}: WIProps) => {
   return (
     <S.InfoContainer>
-      <S.Title>{title}</S.Title>
-      <S.Subtitle>{subtitle}</S.Subtitle>
+      <S.Title isMainContent={isMainContent}>{title}</S.Title>
+      <S.Subtitle isMainContent={isMainContent}>{subtitle}</S.Subtitle>
     </S.InfoContainer>
   );
 }
--- src/components/WeatherInformation/styles.ts
+++ src/components/WeatherInformation/styles.ts
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 
+interface Props {
+  isMainContent?: boolean;
+}
+
 export const InfoContainer = styled.div`
 
 `;
 
-export const Title = styled.span`
-  font-size: 9rem;
+export const Title = styled.span<Props>`
+  font-size: ${props => props.isMainContent ? '9rem' : '4rem'};
   font-weight: 600;
   color: #E7E7EB;
 `;
 
-export const Subtitle = styled.span`
-  font-size: 4rem;
+export const Subtitle = styled.span<Props>`
+  font-size: ${props => props.isMainContent ? '4rem' : '2rem'};
   font-weight: 500;
   color: #A09FB1;
 `;
