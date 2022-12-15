bd085cbc1dfee623a1f2e72eb3df4e9f73eb2b4f
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-15 01:07:03 -0300

    refactor: changed size sidebar

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Container = styled.section`
   width: 100%;
   min-height: 100vh;
-  max-width: 475px;
+  max-width: 459px;
   background-color: #1E213A;
 `;
 
