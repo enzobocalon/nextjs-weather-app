3561cdc54cb0f303ecb5f9c341fcf7c1921799a4
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-15 19:28:47 -0300

    feat: changed button props (box shadow)

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -16,14 +16,14 @@ export const Button = styled.button<Props>`
   align-items: center;
   justify-content: center;
   border: none;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: ${props => props.customBackground ? '' : '0px 4px 4px rgba(0, 0, 0, 0.25);'};
   font-weight: ${props => props.fontWeight ? props.fontWeight : '500'};
   font-family: 'Raleway', sans-serif;
   font-size: ${props => props.fontSize ? `${props.fontSize}px` : '16px'};
   padding: ${props => props.buttonType === 'rounded' ? '' : '10px 18px'};
   border-radius: ${props => props.buttonType === 'rounded' ? '50%' : ''};
   width: ${props => props.buttonType === 'rounded' ? '40px' : 'auto'};
-  height: ${props => props.buttonType === 'rounded' ? '40px' : 'auto'};
+  height: ${props => props.buttonType === 'rounded' ? '40px' : '100%'};
   background-color: ${props => props.customBackground ? props.customBackground : props.isActive ? '#E7E7EB' : '#6E707A'};
   color: ${props => props.isActive ? '#110E3C' : '#E7E7EB;'};
   cursor: pointer;
