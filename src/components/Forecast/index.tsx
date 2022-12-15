986f616a5a881b61109989aec830ae60a439ba73
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-15 16:25:57 -0300

    feat: forecast component

--- src/components/Forecast/index.tsx
+++ src/components/Forecast/index.tsx
@@ -1,6 +1,9 @@
 import Button from '../Button';
 import WeatherCard from '../WeatherCard';
+import WeatherInformation from '../WeatherInformation';
+import { IoMdNavigate } from 'react-icons/io'
 import * as S from './styles'
+import ProgressBar from '../ProgressBar';
 
 const Forecast = () => {
   return (
@@ -22,6 +25,36 @@ const Forecast = () => {
         <WeatherCard />
         <WeatherCard />
       </S.ForecastRow>
+
+      <S.Hightligths>
+        <h2>Today&apos;s Highlights</h2>
+
+        <S.HightligthsGrid>
+          <S.HightlightCard>
+            <span>Wind Status</span>
+            <WeatherInformation title='7' subtitle='mph'/>
+            <S.CardFooter>
+              <S.ArrowContainer>
+                <IoMdNavigate size={20}/>
+              </S.ArrowContainer>
+              <span>WSW</span>
+            </S.CardFooter>
+          </S.HightlightCard>
+          <S.HightlightCard>
+            <span>Humidity</span>
+            <WeatherInformation title='84' subtitle='%'/>
+            <ProgressBar />
+          </S.HightlightCard>
+          <S.HightlightCard>
+            <span>Visibility</span>
+            <WeatherInformation title='6,4' subtitle='miles'/>
+          </S.HightlightCard>
+          <S.HightlightCard>
+            <span>Air Pressure</span>
+            <WeatherInformation title='998' subtitle='mb'/>
+          </S.HightlightCard>
+        </S.HightligthsGrid>
+      </S.Hightligths>
     </S.Container>
   );
 }
--- src/components/Forecast/styles.ts
+++ src/components/Forecast/styles.ts
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  padding: 2rem 9.625rem;
+  padding: 2rem;
+  padding-left: 9.5rem;
+  padding-right: 7.8125rem;
   width: calc(100% - 459px);
 
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 72px;
+
+  @media (max-width: 1100px) {
+    padding: 2rem;
+  }
 `;
 
 export const Header = styled.div`
@@ -19,11 +26,60 @@ export const Header = styled.div`
 `;
 
 export const ForecastRow = styled.div`
+  display: grid;
+  grid-gap: 26px;
+  width: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+`;
+
+export const Hightligths = styled.div`
   width: 100%;
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  gap: 26px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 32px;
+`;
 
-  margin-top: 66px;
+export const HightligthsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(46%, 1fr));
+  grid-template-rows: repeat(2, 1fr);
+  grid-gap: 3rem;
+  height: 100%;
+  width: 100%;
+`;
+
+export const HightlightCard = styled.div`
+  background-color: #1E213A;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 160px;
+  padding-block: 24px;
+  gap: 18px;
+`;
+
+export const CardFooter = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  span {
+    font-size: 14px;
+  }
+`;
+
+export const ArrowContainer = styled.div`
+  background-color: rgba(255, 255, 255, 0.3);
+  width: 30px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+
+  svg {
+    transform: rotate(-150deg);
+  }
 `;
