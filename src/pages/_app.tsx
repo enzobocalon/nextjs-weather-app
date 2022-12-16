7dd447a9eb489b0765b71f2be074561e59528f0a
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-16 16:24:05 -0300

    feat: add weather provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
+import WeatherProvider from '../contexts/Weather'
 import '../styles/_app.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WeatherProvider>
+      <Component {...pageProps} />
+    </WeatherProvider>
+  )
 }
