fb79867621b33a3d2fdda8616c42ce3b634b8363
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-17 13:34:40 -0300

    feat: dynamic image source

--- src/components/WeatherDisplay/index.tsx
+++ src/components/WeatherDisplay/index.tsx
@@ -1,10 +1,11 @@
 import * as S from './styles'
 import Image from "next/image";
 
-import clear from '../../assets/Clear.png'
+import clear from '../../assets/Clear.png' //placeholder
+import { getCondition } from '../../utils/getCondition';
 
 interface WeatherProps {
-  weather?: string;
+  weather?: number;
   isMainWeather: boolean;
 }
 
@@ -12,7 +13,7 @@ const WeatherDisplay = ({weather, isMainWeather}: WeatherProps) => {
   return (
     <>
       <S.Container>
-        <Image src={clear} width={isMainWeather ? 180 : 50} height={isMainWeather ? 180 : 50} alt={'current weather'}/>
+        <Image src={weather ? getCondition(weather) : clear} width={isMainWeather ? 180 : 50} height={isMainWeather ? 180 : 50} alt={'current weather'}/>
       </S.Container>
     </>
   );
