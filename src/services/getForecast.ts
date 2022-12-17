36ac63ccf31a745d93a10e81a295d9d38fe10b28
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-17 20:14:25 -0300

    feat: get searched data from api

--- src/services/getForecast.ts
+++ src/services/getForecast.ts
@@ -1,3 +1,6 @@
-export const getWeather = (location: string) => {
+import axios from "axios"
 
+export const getForecast = async (location: string) => {
+  const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=8cdae16bc23d4715b9e164704221612&q=${location}&days=5&aqi=no&alerts=no`);
+  return response.data;
 }
